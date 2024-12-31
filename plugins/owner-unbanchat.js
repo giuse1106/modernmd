4c8466001abd8d6ab77e4b50d70fbc7113a61ce3
numerofoxi <gabrielepcirpe@gmail.com>
2024-12-31 16:30:45 +0100

    owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,11 @@
+//Crediti by Gabs
+
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('𝐜𝐡𝐚𝐭 𝐬𝐛𝐥𝐨𝐜𝐜𝐚𝐭𝐚 𝐜𝐨𝐧 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐨 ✓')
+m.reply('il bot si è svegliato 🔔')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = /^unbanchat|unbangp$/i
+handler.command = /^on$/i
 handler.rowner = true
 export default handler
