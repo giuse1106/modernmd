fb4594aba3bc802374ba9467d17fea68413ba594
numerofoxi <gabrielepcirpe@gmail.com>
2024-12-31 16:28:22 +0100

    owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,9 +1,11 @@
+//Crediti by Gabs
+
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
-m.reply('𝐜𝐡𝐚𝐭 𝐛𝐥𝐨𝐜𝐜𝐚𝐭𝐚 𝐜𝐨𝐧 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐨 ✓')
+m.reply('il bot si è addormentato 💤')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = /^banchat|bangp$/i
+handler.command = /^banchat|off$/i
 handler.rowner = true
 export default handler
