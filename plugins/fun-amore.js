db81f5f01636a80f04c2e8ac116bbac4341f55b3
numerofoxi <gabrielepcirpe@gmail.com>
2024-12-31 16:13:35 +0100

    fun-amore.js

--- plugins/fun-amore.js
+++ plugins/fun-amore.js
@@ -1,9 +1,14 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `*❤️❤️ CALCOLATORE DI AMORE ❤️❤️*
-*L'amore da parte della tua crush ${text} per te è* *${Math.floor(Math.random() * 100)}%* *di 100%*
-*Perché non ti dichiari ?*
+    let love = `*💘 CALCOLATORE DI AMORE 💘*\n
+━━━━━━━━━━━━━━━━━━━━━
+🌹 *Amore dei tuoi sogni:* ${text}
+💌 *Livello di amore:* *${Math.floor(Math.random() * 101)}%* su *100%*
+━━━━━━━━━━━━━━━━━━━━━
+❓ *Perché non ti dichiari?*
+🤔 *L'amore potrebbe sorprenderti!*
 `.trim()
-m.reply(love, null, { mentions: conn.parseMention(love) })}
+    m.reply(love, null, { mentions: conn.parseMention(love) })
+}
 handler.help = ['love']
 handler.tags = ['fun']
 handler.command = /^(crush)$/i
